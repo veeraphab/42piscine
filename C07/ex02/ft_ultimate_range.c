8f28ae2a93f0b810e817197c4281da9e32794ec7
veeraphab sutthiipibul <veeraphabvv@gmail.com>
2024-06-05 20:11:16 +0700

    add C07 ex02

---
+++ C07/ex02/ft_ultimate_range.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+int ft_ultimate_range(int **range, int min, int max)
+{
+	int i;
+	int size;
+	if (min >= max)
+		return (0);
+	size = max - min;
+	*range = (int *)malloc(size * 4);
+	if (*range == 0)
+		return -1;
+	i = 0;
+	while (min < max)
+		(*range)[i++] = min++;
+	return (size);
+}
+#include <stdio.h>
+int main(void)
+{
+	int min = 5;
+	int max = 10;
+	int *range;
+	int size = ft_ultimate_range(&range, min, max);
+	printf("size  = %d\n", size);
+	for (int i = 0; i < max - min; i++)
+		printf("%d, ", range[i]);
+	free(range);
+	return 0;
+}
