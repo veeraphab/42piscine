2ae1953503bf7d207ee26b4d7ab7247a168ce358
veeraphab sutthiipibul <veeraphabvv@gmail.com>
2024-06-07 00:02:28 +0700

    edits

--- C05/ex07/ft_find_next_prime.c
+++ C05/ex07/ft_find_next_prime.c
@@ -1,17 +1,22 @@
-int	ft_check(int nb, int div)
+int	ft_check(long int nb, long int div)
 {
-	if (div == 1)
+	if (div * div > nb)
 		return (1);
 	if (nb % div == 0)
 		return (0);
-	return (ft_check(nb, div - 1));
+	return (ft_check(nb, div + 1));
 }
+
 int	ft_is_prime(int nb)
 {
-	if (nb <= 1)
+	long int nbl;
+
+	nbl = nb;
+	if (nbl <= 1)
 		return (0);
-	return (ft_check(nb, nb - 1));
+	return (ft_check(nbl, 2));
 }
+
 int	ft_find_next_prime(int nb)
 {
 		if (ft_is_prime(nb) == 1)
